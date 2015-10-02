b8e09db6a1854ed6ccd7c98dc7ad0d8f51df30e4
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2015-10-02 22:47:40 +0430

    Added literals to get the messages

---
+++ locales/literals.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+module.exports = {
+    "NOT_FOUND"         : "Not found",
+    "PAGE_NOT_FOUND"    : "Page not found",
+    "LOGIN_ERROR"       : "User or password incorrect",
+    "TOKEN_ERROR"       : "Invalid token, it could be expired or damaged",
+    "LOGIN_OK"          : "Login successful",
+    "OK"                : "Ok",
+    "ERROR"             : "Unrecognized error",
+    "REQUEST_ERROR"     : "Petition are incorrect"
+};
